7671aeb0c99005c5afb4a667facbfec87d372566
Marvel2410 <139808890+Marvel2410@users.noreply.github.com>
2024-01-12 17:26:46 -0800

    Exercise 5.2

--- components/Chat.js
+++ components/Chat.js
@@ -1,11 +1,38 @@
 //Chat.js
 
-import { useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, View, Text, KeyboardAvoidingView, Platform } from 'react-native';
+import { GiftedChat, Bubble } from "react-native-gifted-chat";
 
 
 const Chat = ({ route, navigation }) => {
   const { name, selectedColor } = route.params;
+  const [messages, setMessages] = useState([]);
+  const onSend = (newMessages) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
+  }
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: "Hello developer",
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+      {
+        _id: 2,
+        text: 'This is a system message',
+        createdAt: new Date(),
+        system: true,
+      },
+    ]);
+  }, []);
+
 
   useEffect(() => {
     navigation.setOptions({
@@ -16,19 +43,75 @@ const Chat = ({ route, navigation }) => {
     });
   }, []);
 
+
   return (
     <View style={[styles.container, { backgroundColor: selectedColor }]}>
-      <Text>Welcome to the Chat!</Text>
+      <View style={styles.welcomeContainer}>
+        <Text style={styles.welcomeText}>Welcome to the Chat!</Text>
+      </View>
+
+      <View style={styles.chatContainer}>
+        <GiftedChat
+          messages={messages}
+          renderBubble={renderBubble}
+          onSend={messages => onSend(messages)}
+          user={{
+            _id: 1
+          }}
+          timeTextStyle={{
+            left: {
+              color: '#000',
+            },
+            right: {
+              color: '#FFF',
+            },
+          }}
+        />
+        {Platform.OS === 'android' ? (
+          <KeyboardAvoidingView behavior="height" />
+        ) : (
+          <KeyboardAvoidingView behavior='padding' />
+        )}
+      </View>
     </View>
   );
 }
 
+const renderBubble = (props) => {
+  return <Bubble
+    {...props}
+    wrapperStyle={{
+      right: {
+        backgroundColor: "#4a90e2",
+      },
+      left: {
+        backgroundColor: "#8bc34a"
+      }
+    }}
+  />
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
+  },
+  chatContainer: {
+    flex: 1,
+  },
+  welcomeContainer: {
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    padding: 20,
+  },
+  welcomeText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'black',
+    textShadowColor: 'rgba(255, 255, 255, 0.5)',
+    textShadowOffset: { width: 2, height: 2 },
+    textShadowRadius: 5,
+
+  },
 });
+
 
 export default Chat;
